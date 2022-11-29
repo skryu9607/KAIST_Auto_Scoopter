34f3a342e11ec5db5515043aecf912f127cdbdf9
HUGO RYU <78149686+skryu9607@users.noreply.github.com>
2022-11-29 19:03:11 +0900

    Third commit

--- Scoopter.py
+++ Scoopter.py
@@ -1,10 +1,4 @@
-'''Global Variables
-    D = [D_0,D_1,D_2,...D_T]
-    D_k [i][j]:  시각 k 에서의 목적지 i로부터 목적지 j까지 가는 예측 수요. 
-    M : 목적지들의 list, 일명 station들의 위치이다. 
-    스쿠터끼리는 같은 모델이므로, 어느 지점에서 어느 지점까지
-    이동할때 필요한 연료 소비량이 같다고 가정한다. 
-
+'''
 Basic Assumptions
 1. 차량은 station에서 대기한다.
 2. 사람은 station에서 탈 수도 있지만, 원하는 도로지점에서 차량을 부를 수 있다.
@@ -19,12 +13,13 @@ Basic Assumptions
 # 1. edge map 과 node map을 지정.
 import csv
 import numpy as np
-no = list()
+import random
+nodd = list()
 f = open('kaist_node.csv','r')
 n = csv.reader(f)
 for row in n:
-    no.append(row)
-node = np.array(no[1:])
+    nodd.append(row)
+node = np.array(nodd[1:])
 
 Node=node[:,0]
 Node_yx = node[:,1:3]
@@ -32,8 +27,17 @@ Node_road_count = node[:,3]
 f.close
 
 # ! kaist_edge.csv했을 때 name이 깨진다. 
+nodd = list()
+ff = open('kaist_edge.csv','r',encoding='utf-8')
+n = csv.reader(ff)
+for row in n:
+    nodd.append(row)
+edge = np.array(nodd[1:])
 
-
+edge_u = edge[:,0]
+edge_v = edge[:,1]
+f.close
+print(edge_u)
 
 '''
 2. Station 지정해주기 <- special node일 뿐
@@ -47,29 +51,63 @@ stations_yx = Node_yx[idx]
 stations_road_count = Node_road_count[idx]
 
 # LIST 1. 수요 예측하는 list 만들기
+
 T = 10
 # Scoopter number is 20
 N = 20 
-demands = np.random.default_rng().dirichlet(np.ones(num-1),size=1)
-demands = [round(v) for v in demands[0]*N]
-print(demands)
+Dem = []
 Demand = []
 for i in range(T):
     for j in range(num):
-        demands = np.random.default_rng().dirichlet(np.ones(num),size=1)
-        demands = [round(v) for v in demands[0]*N]
-        Demand.append(demands)
-print(Demand)
+        demands = N * np.random.default_rng().dirichlet(np.ones(num),size=1)
+        demands = [np.round(v) for v in demands]
+        Dem.append(demands)
+    Dem_ = np.asarray(Dem).reshape(-1,8)
+    Dem = []
+    Demand.append(Dem_)
+Demand = np.array(Demand)
+# 시간대에 따른 수요 <- 재배치에 쓰일 예정
+print(Demand[2])
 
 # 3. node에 연결된 edge 찾는 함수 만들기
+def linked_edge(osmid):
+    # osmid에 해당하는 index number
+    edge_idx = np.where(edge_u == osmid)
+    edge_idx = list(edge_idx)
+    return edge_idx
+print(np.random.choice(Node,1))
+idx = linked_edge(Node[1])
+Node_destination = edge_v[idx]
+print(Node_destination)
+
+# 3.5. distance 함수짜기, node와 node 사이에 연결된 edge를 따라서.
+def distance_nodes(osmid1,osmid2,edge_idx,alpha):
+    # osmid1 : start node
+    # osmid2 : destination node
+    # edge_idx : edges between start node and destination node
+    # alpha : constants
+    start_idx = np.where(Node == osmid1)
+    end_idx = np.where(Node == osmid2)
+    
+
+
 
 ''' 
 4. 위치 지정하는 함수 만들기 
 1) Node를 random하게 결정.
 2) Node에서 연결된 edge를 random하게 결정
 3) 그 edge들에서 parameter를 random하게 결정 [0,1]
-
 '''
+
+def sample_location():
+    rand_node = np.random.choice(Node,1)
+    rand_edge = np.random.choice(linked_edge(rand_node),1)
+    rand_destination_node = edge_v[rand_edge]
+    rand_alpha = random.random()
+    sample_location =  rand_node + rand_alpha * distance(rand_node,rand_destination_node,rand_edge,rand_alpha)
+    return sample_location
+
+
 
 
 # 6. Benefit 함수 짜기
